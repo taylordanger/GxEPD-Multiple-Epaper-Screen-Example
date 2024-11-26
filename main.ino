ece6b1ec5ec84413dd4496c1c90d388166b62f74
TheCatwoman <taylor.sheppard@email.socc.edu>
2024-11-26 11:53:18 -0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,25 +1,25 @@
 #include <GxEPD.h>
-#include <GxGDEM029T94/GxGDEM029T94.h>   // THIS IS IMPORTANT 
+#include <GxGDEM029T94/GxGDEM029T94.h>   
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>  
 #include <SPI.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
 #include <Fonts/FreeMonoBold12pt7b.h>
-#include <SD.h> 
-//pinouts for screen 1
-#define CS1   5 
-#define DC1   22  
-#define RSET1 21  
-#define BUSY1 4  
-// pinouts for screen 2
-#define CS2   15  
-#define DC2   2   
-#define RSET2 0  
-#define BUSY2 16  
+#include <SD.h>  // Include the SD library
 
-#define MOSI 23 
-#define SCLK 18  
+#define CS1   5   // Chip select for display 1
+#define DC1   22  // Data/Command for display 1
+#define RSET1 21  // Reset for display 1
+#define BUSY1 4   // Busy for display 1
 
+#define CS2   15  // Chip select for display 2
+#define DC2   2   // Data/Command for display 2
+#define RSET2 0   // Reset for display 2
+#define BUSY2 16  // Busy for display 2
+
+#define MOSI 23  // Data in
+#define SCLK 18  // Clock
+#define CS_PIN 33 // Chip select for SD card
 
 GxIO_Class io1(SPI, CS1, DC1, RSET1);
 GxEPD_Class display1(io1, RSET1, BUSY1);
@@ -29,18 +29,56 @@ GxEPD_Class display2(io2, RSET2, BUSY2);
 
 const char TextBsp[] = "Irgendein Text";
 
-int cnt;
+int cnt; // counter zum Testen des Beispiels mit alter GxEPD library
+
+File root; // Declare the root variable
+
+void printDirectory(File dir, int numTabs);
 
 void setup() {
-  Serial.begin(9600);
-  
+  Serial.begin(115200);
+ // Initialize displays
   SPI.begin(SCLK, MISO, MOSI);
-  
   display1.init();
   display2.init();
+
+  display1.setRotation(1);
+  display1.fillScreen(GxEPD_WHITE);
+  display1.setTextColor(GxEPD_BLACK);
+  display1.setFont(&FreeMonoBold9pt7b);
+  display1.setCursor(10, 30);
+  display1.print("Initializing SD card... ");
+  display1.update();
+
+
+  if (!SD.begin(CS_PIN)) {
+    Serial.println("Card initialization failed!");
+    while (true);
+  }
+
+  Serial.println("initialization done.");
+
+  Serial.println("Files in the card:");
+  root = SD.open("/");
+  printDirectory(root, 0);
+  Serial.println("");
+
+  // Example of reading file from the card:
+  File textFile = SD.open("/wokwi.txt");
+  if (textFile) {
+    Serial.print("wokwi.txt: ");
+    while (textFile.available()) {
+      Serial.write(textFile.read());
+    }
+    textFile.close();
+  } else {
+    Serial.println("error opening wokwi.txt!");
+  }
+
 }
 
 void loop() {
+  delay(100);
   display1.setRotation(1);
   display1.fillScreen(GxEPD_WHITE);
   display1.setTextColor(GxEPD_BLACK);
@@ -48,7 +86,6 @@ void loop() {
   display1.setCursor(10, 30);
   display1.println("sheppard semiconductor systems");
   display1.setFont(&FreeMonoBold12pt7b);
-  display1.printf("Screen 1 %d", cnt);
   display1.update();
 
   display2.setRotation(1);
@@ -62,5 +99,52 @@ void loop() {
   display2.update();
 
   cnt++;
-  delay(10000);
+  
+  void printDirectory(File dir, int numTabs);
+}
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+    File entry = dir.openNextFile();
+    if (!entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+       display1.setRotation(1);
+        display1.fillScreen(GxEPD_WHITE);
+        display1.setTextColor(GxEPD_BLACK);
+        display1.setFont(&FreeMonoBold9pt7b);
+        display1.setCursor(10, 30);
+        display1.print('\t');
+          display1.update();
+
+    }
+     display1.setRotation(1);
+  display1.fillScreen(GxEPD_WHITE);
+  display1.setTextColor(GxEPD_BLACK);
+  display1.setFont(&FreeMonoBold9pt7b);
+  display1.setCursor(10, 30);
+    display1.print(entry.name());
+      display1.update();
+
+    if (entry.isDirectory()) {
+       display1.setRotation(1);
+  display1.fillScreen(GxEPD_WHITE);
+  display1.setTextColor(GxEPD_BLACK);
+  display1.setFont(&FreeMonoBold9pt7b);
+  display1.setCursor(10, 30);
+      display1.println("/");
+  display1.update();
+
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      display1.print("\t\t");
+      display1.println(entry.size(), DEC);
+        display1.update();
+
+    }
+    entry.close();
+  }
 }
